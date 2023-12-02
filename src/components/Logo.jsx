fa62b906569c669852e33f464b4c366022a8cdd4
jlquijada63 <70599089+jlquijada63@users.noreply.github.com>
2023-12-02 20:16:00 +0100

    Logo component finish

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+
+import styles from './Logo.module.css'
+
+function Logo() {
+  return (
+    <div className="logo-container">
+      <h1 className={styles.h1}>🧳 Far Away 🏝️</h1>
+    </div>
+  )
+}
+export default Logo
