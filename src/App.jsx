495ff79b9ffebf623f85f3b1d0dac623b8657679
jlquijada63 <70599089+jlquijada63@users.noreply.github.com>
2023-12-03 18:17:30 +0100

    localStorage work

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import Logo from "./components/Logo"
 import Form from "./components/Form"
 import PackingList from "./components/PackingList"
 import Stats from "./components/Stats"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Toothpaste", quantity: 2, packed: false },
-  { id: 4, description: "airpods", quantity: 1, packed: false },
-  { id: 5, description: "wallet", quantity: 1, packed: false },
-  { id: 6, description: "shoes", quantity: 2, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Toothpaste", quantity: 2, packed: false },
+//   { id: 4, description: "airpods", quantity: 1, packed: false },
+//   { id: 5, description: "wallet", quantity: 1, packed: false },
+//   { id: 6, description: "shoes", quantity: 2, packed: false },
+// ];
+
+const initialItems = localStorage.getItem('items')
 
 
 function App() {
 
-	const [items, setItems] = useState(initialItems)
+	const [items, setItems] = useState(JSON.parse(initialItems))
+
+	useEffect(() => { 
+
+		localStorage.setItem('items', JSON.stringify(items))
+		
+	 }, [items])
 
 	return (
 		<div className="container">
