c776b589d9bf865caa2545f995e7f90bba94acfa
jlquijada63 <70599089+jlquijada63@users.noreply.github.com>
2023-12-03 11:09:56 +0100

    sort functionality work

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -2,6 +2,38 @@ import Item from "./Item"
 import styles from "./PackingList.module.css"
 
 function PackingList({ items, setItems }) {
+
+
+	const handleChange = (e) => { 
+
+		// sort by input order
+		console.log(e.target.value)
+		const sortedArray = [...items]
+
+		if (e.target.value === "byInput") {
+			sortedArray.sort((a, b) => a.id - b.id)
+			setItems(sortedArray)
+		}else if (e.target.value === "byDescription"){
+			sortedArray.sort((a,b) => {
+				if(a.description.toUpperCase() < b.description.toUpperCase()){
+					return -1
+				}
+				if (a.description.toUpperCase() > b.description.toUpperCase()){
+					return 1
+				}
+				return 0
+			})
+			console.log(sortedArray)
+			setItems(sortedArray)
+			
+		}else{
+			sortedArray.sort ((a,b) => a.packed - b.packed)
+			setItems(sortedArray)
+		}
+	}
+
+
+
 	return (
 		<div className={styles.packingListContainer}>
 			<div className={styles.packingListGrid}>
@@ -15,10 +47,10 @@ function PackingList({ items, setItems }) {
 				))}
 			</div>
 			<div className={styles.btnContainer}>
-				<select className={styles.selectBtn}>
-					<option>SORT BY INPUT ORDER</option>
-					<option>SORT BY DESCRIPTION</option>
-					<option>SORT BY PACKED STATUS</option>
+				<select className={styles.selectBtn} onChange={handleChange}>
+					<option value="byInput">SORT BY INPUT ORDER</option>
+					<option value="byDescription">SORT BY DESCRIPTION</option>
+					<option value="byPackedStatus">SORT BY PACKED STATUS</option>
 				</select>
 				<button className={styles.clearBtn}>CLEAR LIST</button>
 			</div>
